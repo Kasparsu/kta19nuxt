2f00f8f80ecbf1df28620de2d3826132106658ca
Kaspar Suursalu <kasparsu@gmail.com>
2021-04-22 10:14:47 +0300

    init commit

---
+++ pages/cats/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>i HAVE MANY CATS</h1>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/cats/nuustik.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>I love my cat nuustik</h1>
+</template>
+
+<script>
+    export default {
+        name: "nuustik"
+    }
+</script>
+
+<style scoped>
+
+</style>
