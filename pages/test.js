7ac3c368d6b64a3954658614024a842a9b130a97
Kaspar Suursalu <kasparsu@gmail.com>
2021-05-06 11:03:30 +0300

    add gmap

---
+++ pages/test.js
@@ -0,0 +1,16 @@
+fetch("https://paulig.heydaypro.com/frezzapop/api/submit.php", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json;charset=UTF-8",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1"
+  },
+  "referrer": "https://paulig.heydaypro.com/frezzapop/dist/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"name\":\"aaa v.\",\"firstname\":\"aaa\",\"lastname\":\"vvvv\",\"email\":\"asdas@aa.com\",\"marketing\":false,\"cookie\":\"0a4988d8-1e4b-4701-ad3a-36bf3a8d3a9a\",\"team\":\"\",\"score\":14575,\"location\":\"est\"}",
+  "method": "POST",
+  "mode": "cors"
+});
